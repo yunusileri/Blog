cea3cebc114ec7fbb90449e536220e47acccedf2
yunusileri <yunusileri.tr@gmail.com>
2019-04-21 02:19:23 +0300

    veritabani ismi duzeltildi

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
+        'NAME': 'blogdb',
         'USER': 'yunus',
         'PASSWORD': '1',
         'HOST': 'localhost',
